c51865d62a61cacc3258d5f1a1d24b5480bc0a20
雷学伟 <leixw@helianhealth.com>
2018-05-24 09:31:26 +0800

    添加可编辑的 HTML 输入框。
    Signed-off-by: 雷学伟 <leixw@helianhealth.com>

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
     <div v-html="result"></div>
     <hr/>
     <div>{{original}}</div>
+    <hr/>
+    <button @click="addEmojiHandler('😠')">
+      <img alt="😠"
+           class="row-text"
+           src="/static/emoji/angry.png" />
+    </button>
+    <div class="box"
+         ref="content"
+         @paste="pasteHandler"
+         contentEditable="true">
+    </div>
   </div>
 </template>
 
@@ -15,7 +26,7 @@ const imgRegex = /<img\ssrc="[A-Za-z0-9/.-]*?"\sclass="row-text"\salt="(.*?)"\s\
 
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
       result: '',
       original: '',
@@ -29,7 +40,7 @@ export default {
       }
     }
   },
-  mounted () {
+  mounted() {
     const regex = emojiRegex()
 
     this.result = this.text.replace(regex, v => {
@@ -41,9 +52,17 @@ export default {
       }
     })
 
-    console.log(this.result)
-
     this.original = this.result.replace(imgRegex, '$1')
+  },
+  methods: {
+    addEmojiHandler(v) {
+      const html = `<img src="/static/emoji/${this.mapper[v]}.png" class="row-text" alt="${v}" />`
+
+      this.$refs.content.innerHTML += html
+    },
+    pasteHandler(e) {
+      debugger
+    }
   }
 }
 </script>
@@ -52,5 +71,15 @@ export default {
 .row-text {
   /* width: 20px;
   height: 20px; */
+}
+
+.box {
+  width: 400px;
+  min-height: 100px;
+  max-height: 300px;
+  height: auto;
+  outline: none;
+  border: 1px solid #f00;
+  overflow-y: auto;
 }
 </style>
